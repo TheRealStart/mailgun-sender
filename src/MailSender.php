89fd5dc99a9e966a8985356b18cdbe54519aa3ea
Igor Bronnicov <ibronnicov@gmail.com>
2019-06-21 12:50:18 +0500

    base code

---
+++ src/MailSender.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MailgunSender;
+
+
+use Exception;
+use Mailgun\Mailgun;
+
+class MailSender
+{
+    private $key;
+    private $domain;
+
+    public function __construct($key, $domain)
+    {
+        $this->key = $key;
+        $this->domain = $domain;
+    }
+
+    public function send($from, $to, $subject, $message)
+    {
+        try {
+            $mg = Mailgun::create($this->key);
+            $mg->messages()->send($this->domain, [
+                'from'    => $from,
+                'to'      => $to,
+                'subject' => $subject,
+                'text'    => $message
+            ]);
+        } catch (Exception $e) {
+            echo "<p>{$e->getMessage()}</p>";
+        }
+    }
+}
